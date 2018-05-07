042b948d3dac98c1ba75bac81ec84afd840da4f5
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-07 11:18:31 +0200

    first skeleton

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ const divMain = document.getElementById('main')
 
 const render = (html) => divMain.innerHtml = html
 
+const content = `
+<b>Bonjour sympathiques visiteurs</b>
+`
+	
+render(content)
