50297d87c7f9d7ba3be2cb56a1e15a402dbe0b96
Unknown <tarteserviceinformatique@sfr.fr>
2018-07-10 13:50:25 +0200

    fill main

--- main.js
+++ main.js
@@ -3,7 +3,66 @@ const divMain = document.getElementById('main')
 const render = (html) => divMain.innerHtml = html
 
 const content = `
-<b>Bonjour sympathiques visiteurs</b>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CONCEPTION / DEVELOPPEMENT
+
+Environnement windows, linux Release Debian (Ubuntu)
+Méthodologie : Scrum, UML (logiciel : Open ModelSphere), Merise
+Architecture : M.V.C. (Modèle Vue Contrôle)
+Langages : Javascript-ES6/React, Php (logiciel : eclipse), DotNet (logiciel : visual studio), java, SQL
+Connaissances : HTML5, CSS3, BOOTSTRAP, Json,   Jquery, XML, XSD, XSLT, SGBT
+
+
+Langues : Anglais : Lu, parlé et écrit, Allemand : Scolaire
+
+
+
+
+
+
+2018 :		Projet Gyvit Pour la société Meduza
+
+2015 : 	Référenceur Google  Pour VECRSVP à Clacy-et-Thierret (02)
+
+2011-2015 : 	Travailleur Indépendant en informatique Pour les particuliers et les entreprises au Collet de Dèze (48)
+
+2010 :		Période en développement Web (Php) pour le CCAS de la mairie de Cormontreuil
+
+1999-2009 : 	Technicien Informatique
+		Pour diverses société marnaises, parisiennes, lozériennes
+
+
+
+
+
+
+
+
+2018		Ecole Wild Code School de Reims
+			BTS Développeur Javascript/React
+
+2009-2010 :	Centre A.F.P.A. de Marne-la-vallée (77)  
+			Licence Pro Concepteur/Développeur en Informatique
+
+2000 -2001:	Centre A.F.P.A. Yves BODIGEL de Meudon la Forêt (92)
+			BTS Technicien Supérieur en Maintenance et Services en Informatique
+
+1990-1996 :	U.F.R. Sciences de REIMS (51)
+			Licence E.E.A.
 `
 	
 render(content)
